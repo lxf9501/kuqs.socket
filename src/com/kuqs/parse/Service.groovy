ab9740100d1a8dc654a5d1cff09b9879fa505653
alan <lxf9501@gmail.com>
2012-04-17 23:21:01 +0800

    socket

--- src/com/kuqs/parse/Service.groovy
+++ src/com/kuqs/parse/Service.groovy
@@ -23,12 +23,25 @@ class Service {
     }
 
     def client() {
-        Socket s = new Socket("localhost", 4444);
+        Socket s = new Socket("localhost", 3490);
         s.withStreams { input, output ->
-            output << "echo testing ...\n"
-            def buffer = input.newReader().readLine()
+//            output << "echo testing ...\n"
+            DataOutputStream dos = new DataOutputStream(output)
+            byte[] sents = [0x00, 0x01, 0x88] as byte[]
+//            dos.write(0x00)
+//            dos.write(0x01)
+//            dos.write(0x88)
+            dos.write(sents)
+            dos.flush()
+            byte[] buffers = new byte[3]
+            def buffer = input.read(buffers)
             println "response = $buffer"
+            println buffers
         }
 
+    }
+
+    static void main(args) throws Exception {
+        ((new Service()).client())
     }
 }
